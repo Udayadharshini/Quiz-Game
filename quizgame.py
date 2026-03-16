259f76ea080ca2a584c543802c9385200f2f92e9
Udaya Dharshini. S <udayadharshini25@gmail.com>
2026-03-16 22:25:59 +0530

    Update quizgame.py

--- quizgame.py
+++ quizgame.py
@@ -1,10 +1,10 @@
 import random
-from questions import questions  # import questions from questions.py
+from questions import questions  
 
 score = 0
 points_per_question = 20
 
-# Welcome message
+
 print("🎉 WELCOME TO QUIZ 🎉")
 print("Choose Category:\n1. GK\n2. Python\n3. AI")
 choice = input("Enter choice (1-3): ")
@@ -19,10 +19,10 @@ else:
     print("Invalid choice")
     exit()
 
-# Pick 5 random questions
+
 selected = random.sample(questions[category], 5)
 
-# Ask questions
+
 for num, q in enumerate(selected, start=1):
     print(f"\nQ{num}: {q['question']}")
     for opt in q["options"]:
@@ -34,7 +34,6 @@ for num, q in enumerate(selected, start=1):
     else:
         print(f"❌ Wrong! Current Score: {score}/{num*points_per_question}")
 
-# Final Score
 total_score = len(selected) * points_per_question
 print("\n🎯 Quiz Finished!")
 print(f"Your Final Score: {score}/{total_score}")
